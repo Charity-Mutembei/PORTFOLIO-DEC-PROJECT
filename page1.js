54aa6514cc18a7055f1c1556bf45bed3f2250c15
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2022-11-29 01:22:18 +0300

    Add third page, styled and device responsive

---
+++ page1.js
@@ -0,0 +1,39 @@
+const typSpd = 70; 
+const waitTime = 500;
+
+const text = [
+  "Hey there",
+  "Thought I should let you know I am your link to this project",
+]
+
+var mi = 0;
+
+function writeString(e, str, i) {
+  e.innerHTML = e.innerHTML + str[i];
+  
+  if (e.innerHTML.length == str.length && mi != text.length)
+    setTimeout(slowlyDelete, waitTime, e);
+}
+
+function deleteString(e) {
+  e.innerHTML = e.innerHTML.substring(0, e.innerHTML.length - 1);
+  
+  if (e.innerHTML.length == 0)
+    slowlyWrite(e, text[mi++]);
+}
+
+function slowlyDelete(e) {
+  for (var i = 0; i < e.innerHTML.length; i++) {
+    setTimeout(deleteString, typSpd / 2 * i, e);
+  }
+}
+
+function slowlyWrite(e, str) {
+  for (var i = 0; i < str.length; i++) {
+    setTimeout(writeString, typSpd * i, e, str, i);
+  }
+}
+
+const msg = document.querySelector(".msg-icn");
+
+slowlyDelete(msg)
